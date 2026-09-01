0832ff7553d83e0b594f179e0f030986ca87d419
B-priyam <priyam3801h@gmail.com>
2026-09-01 23:39:18 +0530

    fix

---
+++ components/MetaPixel.tsx
@@ -0,0 +1,59 @@
+"use client";
+
+import Script from "next/script";
+import { usePathname, useSearchParams } from "next/navigation";
+import { useEffect } from "react";
+
+declare global {
+  interface Window {
+    fbq: any;
+  }
+}
+
+export function MetaPixel({ pixelId }: { pixelId: string }) {
+  const pathname = usePathname();
+  const searchParams = useSearchParams();
+
+  // Track page views on route changes (for SPA navigation)
+  useEffect(() => {
+    if (pathname && window.fbq) {
+      window.fbq("track", "PageView");
+    }
+  }, [pathname, searchParams]);
+
+  return (
+    <>
+      {/* Load the Meta Pixel script */}
+      <Script
+        id="meta-pixel"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            !function(f,b,e,v,n,t,s){
+              if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+              n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+              if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+              n.queue=[];t=b.createElement(e);t.async=!0;
+              t.src=v;s=b.getElementsByTagName(e)[0];
+              s.parentNode.insertBefore(t,s)
+            }(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '${pixelId}');
+            fbq('track', 'PageView');
+          `,
+        }}
+      />
+
+      {/* Fallback noscript for very old browsers */}
+      <noscript>
+        <img
+          height="1"
+          width="1"
+          style={{ display: "none" }}
+          src={`https://www.facebook.com/tr?id=${pixelId}&ev=PageView&noscript=1`}
+          alt=""
+        />
+      </noscript>
+    </>
+  );
+}
